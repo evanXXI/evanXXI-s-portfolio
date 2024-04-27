3738587bb39165df24ad3a778951632b907007a6
Evan <evan.rktcisse@gmail.com>
2024-04-27 03:23:10 +0200

    :recycle: reorganized main.js
    reorganized main.js to ensure that it runs only when all the content is loaded on the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,40 @@
 /**
-* Template Name: Personal
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Updated: Mar 17 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+ * Template Name: Personal
+ * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
+ * Updated: Mar 17 2024 with Bootstrap v5.3.3
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
 
-(function() {
+window.addEventListener('load', () => {
   "use strict";
 
   /**
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim()
+    el = el.trim();
     if (all) {
-      return [...document.querySelectorAll(el)]
+      return [...document.querySelectorAll(el)];
     } else {
-      return document.querySelector(el)
+      return document.querySelector(el);
     }
-  }
+  };
 
   /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+    let selectEl = select(el, all);
 
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach(e => e.addEventListener(type, listener));
       } else {
-        selectEl.addEventListener(type, listener)
+        selectEl.addEventListener(type, listener);
       }
     }
-  }
+  };
 
   /**
    * Scrolls to an element with header offset
@@ -43,101 +43,99 @@
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
-    })
-  }
+    });
+  };
 
   /**
    * Mobile nav toggle
    */
   on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+    select('#navbar').classList.toggle('navbar-mobile');
+    this.classList.toggle('bi-list');
+    this.classList.toggle('bi-x');
+  });
 
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
   on('click', '#navbar .nav-link', function(e) {
-    let section = select(this.hash)
+    let section = select(this.hash);
     if (section) {
-      e.preventDefault()
+      e.preventDefault();
 
-      let navbar = select('#navbar')
-      let header = select('#header')
-      let sections = select('section', true)
-      let navlinks = select('#navbar .nav-link', true)
+      let navbar = select('#navbar');
+      let header = select('#header');
+      let sections = select('section', true);
+      let navlinks = select('#navbar .nav-link', true);
 
       navlinks.forEach((item) => {
-        item.classList.remove('active')
-      })
+        item.classList.remove('active');
+      });
 
-      this.classList.add('active')
+      this.classList.add('active');
 
       if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
+        navbar.classList.remove('navbar-mobile');
+        let navbarToggle = select('.mobile-nav-toggle');
+        navbarToggle.classList.toggle('bi-list');
+        navbarToggle.classList.toggle('bi-x');
       }
 
       if (this.hash == '#header') {
-        header.classList.remove('header-top')
+        header.classList.remove('header-top');
         sections.forEach((item) => {
-          item.classList.remove('section-show')
-        })
+          item.classList.remove('section-show');
+        });
         return;
       }
 
       if (!header.classList.contains('header-top')) {
-        header.classList.add('header-top')
+        header.classList.add('header-top');
         setTimeout(function() {
           sections.forEach((item) => {
-            item.classList.remove('section-show')
-          })
-          section.classList.add('section-show')
+            item.classList.remove('section-show');
+          });
+          section.classList.add('section-show');
 
         }, 350);
       } else {
         sections.forEach((item) => {
-          item.classList.remove('section-show')
-        })
-        section.classList.add('section-show')
+          item.classList.remove('section-show');
+        });
+        section.classList.add('section-show');
       }
 
-      scrollto(this.hash)
+      scrollto(this.hash);
     }
-  }, true)
+  }, true);
 
   /**
    * Activate/show sections on load with hash links
    */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      let initial_nav = select(window.location.hash)
+  if (window.location.hash) {
+    let initial_nav = select(window.location.hash);
 
-      if (initial_nav) {
-        let header = select('#header')
-        let navlinks = select('#navbar .nav-link', true)
+    if (initial_nav) {
+      let header = select('#header');
+      let navlinks = select('#navbar .nav-link', true);
 
-        header.classList.add('header-top')
+      header.classList.add('header-top');
 
-        navlinks.forEach((item) => {
-          if (item.getAttribute('href') == window.location.hash) {
-            item.classList.add('active')
-          } else {
-            item.classList.remove('active')
-          }
-        })
+      navlinks.forEach((item) => {
+        if (item.getAttribute('href') == window.location.hash) {
+          item.classList.add('active');
+        } else {
+          item.classList.remove('active');
+        }
+      });
 
-        setTimeout(function() {
-          initial_nav.classList.add('section-show')
-        }, 350);
+      setTimeout(function() {
+        initial_nav.classList.add('section-show');
+      }, 350);
 
-        scrollto(window.location.hash)
-      }
+      scrollto(window.location.hash);
     }
-  });
+  }
 
   /**
    * Skills animation
@@ -150,10 +148,10 @@
       handler: function(direction) {
         let progress = select('.progress .progress-bar', true);
         progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
+          el.style.width = el.getAttribute('aria-valuenow') + '%';
         });
       }
-    })
+    });
   }
 
   /**
@@ -177,7 +175,6 @@
         slidesPerView: 1,
         spaceBetween: 20
       },
-
       1200: {
         slidesPerView: 3,
         spaceBetween: 20
@@ -188,30 +185,27 @@
   /**
    * Porfolio isotope and filter
    */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
+  let portfolioContainer = select('.portfolio-container');
+  if (portfolioContainer) {
+    let portfolioIsotope = new Isotope(portfolioContainer, {
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+
+    let portfolioFilters = select('#portfolio-flters li', true);
+
+    on('click', '#portfolio-flters li', function(e) {
+      e.preventDefault();
+      portfolioFilters.forEach(function(el) {
+        el.classList.remove('filter-active');
       });
+      this.classList.add('filter-active');
 
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-      }, true);
-    }
-
-  });
+      portfolioIsotope.arrange({
+        filter: this.getAttribute('data-filter')
+      });
+    }, true);
+  }
 
   /**
    * Initiate portfolio lightbox 
@@ -251,4 +245,4 @@
    */
   new PureCounter();
 
-})()
+});
